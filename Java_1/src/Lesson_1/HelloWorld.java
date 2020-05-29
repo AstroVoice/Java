b9a1ab203885e966965bc01e844d9ab9b5ac2616
AstroVoice <alexander.strugovshchikov@gmail.com>
2020-05-29 20:33:47 +0300

    DunaGameCommit1

---
+++ Java_1/src/Lesson_1/HelloWorld.java
@@ -0,0 +1,8 @@
+package Lesson_1;
+
+public class HelloWorld {
+    public static void main (String[] args) {
+        System.out.print("Hello world!!!\n");
+        System.out.println("Hello world!!!");
+    }
+}
